e411d4335a600dc172e136cbc198c57adc562100
黑风夜 <1149385543@qq.com>
2020-05-25 17:09:37 +0800

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
         <div slot="footer" class="dialog-footer">
             <el-button @click="dialogFormVisible = false">取 消</el-button>
             <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-        </div>
+        </div> 
     </el-dialog>
 </div>
 
